8eeb5818cda66feb44235c71f8eb0a19b7640257
unknown <rounakknth@gmail.com>
2024-11-13 18:44:10 +0530

    update

---
+++ array.js
@@ -0,0 +1,17 @@
+// let a = [1,2,3,4,5,6,7]
+// console.log(a[3])
+// console.log(a[0])
+// let [b,c,v,x,z]=a
+// console.log(z)
+
+
+let a = [1,2,3,4,5,6,7]
+// let b=a.push(23,67,78)
+// console.log(a)
+
+let b=a.pop()
+// console.log(a)
+
+//  let b=a.unshift(67)
+// let b = a.shift()
+console.log(a)
---
+++ var.js
@@ -0,0 +1,23 @@
+// normal output check
+// console.log("hello world")
+
+
+// global scope of variable
+// let a=12
+// function hello(){
+//     console.log(a)
+//     {
+//         // let a=10
+//         console.log(a)
+//     }
+// }
+// hello()
+// console.log(a)
+
+
+// local scope of variable
+function hello(){
+    let a =12
+    console.log(a)
+}
+hello()
