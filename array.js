6fbce8515e860a859d3bf1fcd98c0e6a924a144e
unknown <rounakknth@gmail.com>
2024-11-18 12:03:01 +0530

    update

--- array.js
+++ array.js
@@ -5,13 +5,20 @@
 // console.log(z)
 
 
-let a = [1,2,3,4,5,6,7]
+// let a = [1,2,3,4,5,6,7]
 // let b=a.push(23,67,78)
 // console.log(a)
 
-let b=a.pop()
+// let b=a.pop()
 // console.log(a)
 
 //  let b=a.unshift(67)
 // let b = a.shift()
-console.log(a)
+// console.log(a)
+
+// slice from the array
+
+let a = [1,2,3,4,5,6,7,8]
+// slice(index of the starting slice element,position of the element)
+let b = a.slice(0,4)
+console.log(b)
---
+++ array2.js
@@ -0,0 +1,39 @@
+// slice from the array
+
+let a = [1, 2, 3, 4, 5, 6, 7, 8]
+
+// slice(index of the starting slice element,position of the element)
+    // let b=a.slice(1,5)
+    // // let p=a.slice(2,7)
+    // console.log(b)
+
+
+
+
+// splice from the array..........................
+
+
+
+// splice(index of the starting splice point ,number of element positions we want to splice)
+    // let b=a.splice(3,2)
+    // console.log(b)
+
+
+// to replace the splice elements
+    let r=a.splice(3,2,"ab","b")
+    console.log(a)
+
+
+// to add element at any place using splice()
+    // let r = a.splice(4, 0, "ab")
+    // console.log(a)
+
+
+// primitive data types = null,undefined,number,string,BigInt,boolean,symbol
+// non-primitive data types = object,array,function
+
+    // typeof() = to know the data type of the variable
+
+
+// bigint(long/large integer)
+// ex- let b=2345n
